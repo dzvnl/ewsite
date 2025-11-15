dc8446c8f8540d818d9d7813475137dab9f78ed1
dzvnl <spotifydaniel525@gmail.com>
2025-11-15 14:56:08 -0500

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -1,33 +1,38 @@
-// --- This is a TEST playlist ---
-// I've put your Netlify URL in. You don't need to change anything.
+// --- This is the FINAL test ---
+// I've put your Netlify URL in, just like before.
 
 const baseURL = "https://6918d613edf05dd80c139242--ewsmusic.netlify.app/";
 
-// We are only testing these 4 songs that we KNOW the filenames for.
+// --- THE FIX IS HERE ---
+// I am now adding "music/" before every filename.
+//
+// !!! If your folder is NOT named "music",
+// just change "music/" to "YOUR_FOLDER_NAME/" in the 4 songs below.
+
 const playlist = [
     {
         title: "Digital Bath",
         artist: "Deftones",
-        src: baseURL + "digitalbath.mp3",
-        cover: baseURL + "deftones.jpg"
+        src: baseURL + "music/digitalbath.mp3",
+        cover: baseURL + "music/deftones.jpg"
     },
     {
         title: "Xtal",
         artist: "Aphex Twin",
-        src: baseURL + "xtal.mp3",
-        cover: baseURL + "aphex.jpg"
+        src: baseURL + "music/xtal.mp3",
+        cover: baseURL + "music/aphex.jpg"
     },
     {
         title: "I Don't Wanna Be Me",
         artist: "Type O Negative",
-        src: baseURL + "idontwannabeme.mp3",
-        cover: baseURL + "ton.jpg"
+        src: baseURL + "music/idontwannabeme.mp3",
+        cover: baseURL + "music/ton.jpg"
     },
     {
         title: "Bite My Lip",
         artist: "fakemink",
-        src: baseURL + "bitemylip.mp3",
-        cover: baseURL + "fakemink.jpg"
+        src: baseURL + "music/bitemylip.mp3",
+        cover: baseURL + "music/fakemink.jpg"
     }
 ];
 
