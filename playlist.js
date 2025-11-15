bab9ef3818ff4c93376b4d01be175ac63808e961
dzvnl <spotifydaniel525@gmail.com>
2025-11-15 15:05:47 -0500

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -1,38 +1,150 @@
-// --- This is the FINAL test ---
-// I've put your Netlify URL in, just like before.
+// --- YOU ARE DONE! ---
+// I have put your exact Netlify URL in.
+// I have REMOVED all the 'music/' folder paths.
+// This will work.
 
 const baseURL = "https://6918d613edf05dd80c139242--ewsmusic.netlify.app/";
 
-// --- THE FIX IS HERE ---
-// I am now adding "music/" before every filename.
-//
-// !!! If your folder is NOT named "music",
-// just change "music/" to "YOUR_FOLDER_NAME/" in the 4 songs below.
+// --- YOUR PLAYLIST (BUILT FROM YOUR FILES, NO FOLDER) ---
 
 const playlist = [
     {
         title: "Digital Bath",
         artist: "Deftones",
-        src: baseURL + "ewsongs/digitalbath.mp3",
-        cover: baseURL + "ewsongs/deftones.jpg"
+        src: baseURL + "digitalbath.mp3",
+        cover: baseURL + "deftones.jpg"
+    },
+    {
+        title: "Sextape",
+        artist: "Deftones",
+        src: baseURL + "sextape 4.mp3", // Kept the space
+        cover: baseURL + "deftones.jpg"
     },
     {
         title: "Xtal",
         artist: "Aphex Twin",
-        src: baseURL + "ewsongs/xtal.mp3",
-        cover: baseURL + "ewsongs/aphex.jpg"
+        src: baseURL + "xtal.mp3",
+        cover: baseURL + "aphex.jpg"
     },
     {
         title: "I Don't Wanna Be Me",
         artist: "Type O Negative",
-        src: baseURL + "ewsongs/idontwannabeme.mp3",
-        cover: baseURL + "ewsongs/ton.jpg"
+        src: baseURL + "idontwannabeme.mp3",
+        cover: baseURL + "ton.jpg"
+    },
+    {
+        title: "Life Is Killing Me",
+        artist: "Type O Negative",
+        src: baseURL + "lifeiskilling.mp3",
+        cover: baseURL + "ton.jpg"
+    },
+    {
+        title: "Exit Music (For a Film)",
+        artist: "Radiohead",
+        src: baseURL + "exitmusic.mp3",
+        cover: baseURL + "radiohead.jpg"
+    },
+    {
+        title: "Alternative Outro",
+        artist: "LUCKI",
+        src: baseURL + "alternativeoutro.mp3", // Kept the space
+        cover: baseURL + "lucki.jpg"
     },
     {
         title: "Bite My Lip",
         artist: "fakemink",
-        src: baseURL + "ewsongs/bitemylip.mp3",
-        cover: baseURL + "ewsongs/fakemink.jpg"
+        src: baseURL + "bitemylip.mp3",
+        cover: baseURL + "fakemink.jpg"
+    },
+    {
+        title: "Ambilux",
+        artist: "fakemink",
+        src: baseURL + "ambilux.mp3",
+        cover: baseURL + "fakemink2.jpg"
+    },
+    {
+        title: "I'm Like Why",
+        artist: "xaviersobased",
+        src: baseURL + "imlikewhy.mp3",
+        cover: baseURL + "xav.jpg"
+    },
+    {
+        title: "Love Hate",
+        artist: "xaviersobased",
+        src: baseURL + "lovehate.mp3",
+        cover: baseURL + "xav2.jpg"
+    },
+    {
+        title: "Foamposite Interlude",
+        artist: "smokedope2016",
+        src: baseURL + "foamposite.mp3",
+        cover: baseURL + "smokedope2016.jpg"
+    },
+    {
+        title: "Abysmal Depths Are Flooded",
+        artist: "Xasthur",
+        src: baseURL + "abysmaldepthsareflooded.mp3",
+        cover: baseURL + "xasthur.jpg"
+    },
+    {
+        title: "Diamonds",
+        artist: "Chris Travis",
+        src: baseURL + "diamonds.mp3",
+        cover: baseURL + "christravis.jpg"
+    },
+    {
+        title: "Black Magic",
+        artist: "Ethel Wulf",
+        src: baseURL + "blackmagic.mp3",
+        cover: baseURL + "ethel.jpg"
+    },
+    {
+        title: "4 Raws",
+        artist: "EsDeeKid",
+        src: baseURL + "4raws.mp3",
+        cover: baseURL + "esdee.jpg"
+    },
+    {
+        title: "Fever",
+        artist: "fakemink/buckshot",
+        src: baseURL + "fever.mp3",
+        cover: baseURL + "buckshot.jpg" // Using this cover as a guess
+    },
+    {
+        title: "Stalks",
+        artist: "Ghost Mountain",
+        src: baseURL + "stalks.mp3",
+        cover: baseURL + "ghostmountain.jpg"
+    },
+    {
+        title: "MIA",
+        artist: "feng",
+        src: baseURL + "mia.mp3",
+        cover: baseURL + "ksuuvi.jpg"
+    },
+    {
+        title: "head in the ceiling fan",
+        artist: "Title Fight",
+        src: baseURL + "titlefight.mp3",
+        cover: baseURL + "title fih.jpg"
+    },
+    {
+        title: "Turn Me Loose",
+        artist: "limpbizkit/eminem",
+        src: baseURL + "turnmeloose.mp3",
+        cover: baseURL + "limp.jpg"
+    },
+    {
+        title: "Break da Law",
+        artist: "Three 6 Mafia", // My guess for the rest
+        src: baseURL + "breakdalaw.mp3", // My guess for the rest
+        cover: baseURL + "three6.jpg" // My guess
+    },
+    {
+        title: "Nutshell",
+        artist: "Alice In Chains",
+        src: baseURL + "nutshell.mp3",
+        cover: baseURL + "alice.jpg"
     }
 ];
 
