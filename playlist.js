ba2267317a287a472a8c2d9dfd7b344197212d8c
dzvnl <spotifydaniel525@gmail.com>
2025-11-15 14:54:36 -0500

    Update playlist.js

--- playlist.js
+++ playlist.js
@@ -1,25 +1,14 @@
-// --- YOU ARE DONE! ---
-// I have put your exact Netlify URL in. You don't need to change anything.
-// This list is built from the files you just sent me.
-//
-// NOTE: I fixed the code to work with the "/" at the end of your URL.
-// It will now work perfectly.
+// --- This is a TEST playlist ---
+// I've put your Netlify URL in. You don't need to change anything.
 
 const baseURL = "https://6918d613edf05dd80c139242--ewsmusic.netlify.app/";
 
-// --- YOUR PLAYLIST (BUILT FROM YOUR FILES) ---
-
+// We are only testing these 4 songs that we KNOW the filenames for.
 const playlist = [
     {
         title: "Digital Bath",
         artist: "Deftones",
         src: baseURL + "digitalbath.mp3",
-        cover: baseURL + "deftones.jpg"
-    },
-    {
-        title: "Sextape",
-        artist: "Deftones",
-        src: baseURL + "sextape.mp3", // Used your filename with the space
         cover: baseURL + "deftones.jpg"
     },
     {
@@ -35,106 +24,10 @@ const playlist = [
         cover: baseURL + "ton.jpg"
     },
     {
-        title: "Life Is Killing Me",
-        artist: "Type O Negative",
-        src: baseURL + "lifeiskilling.mp3", // Used your filename
-        cover: baseURL + "ton.jpg"
-    },
-    {
-        title: "Exit Music (For a Film)",
-        artist: "Radiohead",
-        src: baseURL + "exitmusic.mp3",
-        cover: baseURL + "radiohead.jpg"
-    },
-    {
-        title: "Alternative Outro",
-        artist: "LUCKI",
-        src: baseURL + "alternativeoutro.mp3", // Used your filename with the space
-        cover: baseURL + "lucki.jpg"
-    },
-    {
         title: "Bite My Lip",
         artist: "fakemink",
         src: baseURL + "bitemylip.mp3",
         cover: baseURL + "fakemink.jpg"
-    },
-    {
-        title: "Ambilux",
-        artist: "fakemink",
-        src: baseURL + "ambilux.mp3",
-        cover: baseURL + "fakemink2.jpg"
-    },
-    {
-        title: "I'm Like Why",
-        artist: "xaviersobased",
-        src: baseURL + "imlikewhy.mp3",
-        cover: baseURL + "xav.jpg"
-    },
-    {
-        title: "Love Hate",
-        artist: "xaviersobased",
-        src: baseURL + "lovehate.mp3",
-        cover: baseURL + "xav2.jpg"
-    },
-    {
-        title: "Foamposite Interlude",
-        artist: "smokedope2016",
-        src: baseURL + "foamposite.mp3",
-        cover: baseURL + "smokedope2016.jpg"
-    },
-    {
-        title: "Abysmal Depths Are Flooded",
-        artist: "Xasthur",
-        src: baseURL + "abysmaldepthsareflooded.mp3",
-        cover: baseURL + "xasthur.jpg"
-    },
-    {
-        title: "Diamonds",
-        artist: "Chris Travis",
-        src: baseURL + "diamonds.mp3",
-        cover: baseURL + "chris travis.jpg"
-    },
-    {
-        title: "Black Magic",
-        artist: "Ethel Wulf",
-        src: baseURL + "blackmagic.mp3",
-        cover: baseURL + "ethel.jpg"
-    },
-    {
-        title: "4 Raws",
-        artist: "EsDeeKid", // My guess
-        src: baseURL + "4raws.mp3",
-        cover: baseURL + "esdee.jpg"
-    },
-    {
-        title: "Fever",
-        artist: "fakemink/buckshot", // My guess
-        src: baseURL + "fever.mp3",
-        cover: baseURL + "ghostmountain.jpg" // Guessing
-    },
-    {
-        title: "Stalks",
-        artist: "Ghost Mountain", // My guess
-        src: baseURL + "stalks.mp3",
-        cover: baseURL + "ghostmountain.jpg"
-    },
-    {
-        title: "MIA",
-        artist: "ksuuvi", // My guess
-        src: baseURL + "mia.mp3",
-        cover: baseURL + "ksuuvi.jpg"
-    },
-    {
-        title: "Title Fight",
-        artist: "Title Fight", // My guess
-        src: baseURL + "titlefight.mp3",
-        cover: baseURL + "title fih.jpg"
-    },
-    {
-        title: "Turn Me Loose",
-        artist: "Feng?", // My guess
-        src: baseURL + "turnmeloose.mp3",
-        cover: baseURL + "feng.jpg"
     }
 ];
 
